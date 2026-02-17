33298605f490b823a79dba29cfcdcd09937a5f80
Miguel Angel Chata Mamani <miguellangel.cm@gmail.com>
2026-02-17 15:00:52 +0000

    fix it deploy issues

--- src/app/(app)/practice/journal/page.tsx
+++
@@ -1,6 +0,0 @@
-
-import { redirect } from 'next/navigation';
-
-export default function PracticeJournalRedirectPage() {
-  redirect('/practice');
-}
--- src/app/(auth)/login/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function DeprecatedLoginPage() {
-  redirect('/login');
-}
